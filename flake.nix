eead80c294ef748fa521c1db0126559c3e8b9acd
John Lewis <github@jlewis.sh>
2024-03-27 21:11:28 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.557010.tar.gz";
+    rust-overlay = {
+      url = "https://flakehub.com/f/oxalica/rust-overlay/0.1.1330.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        });
+
+        nativeBuildInputs = [ toolchain pkgs.pkg-config ];
+        buildInputs = with pkgs; [
+          udev alsa-lib vulkan-loader
+          libxkbcommon wayland
+        ];
+
+      in {
+        devShell = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+      }
+  );
+}
