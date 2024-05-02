d4d7613316e1e25ba1c748bcfae622b730aa87b9
John Lewis <github@jlewis.sh>
2024-05-02 13:14:32 +0200

    chore(nix): update nix deps

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.557010.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.558121.tar.gz";
     rust-overlay = {
-      url = "https://flakehub.com/f/oxalica/rust-overlay/0.1.1330.tar.gz";
+      url = "https://flakehub.com/f/oxalica/rust-overlay/0.1.1367.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     wgsl-analyzer.url = "github:wgsl-analyzer/wgsl-analyzer";
